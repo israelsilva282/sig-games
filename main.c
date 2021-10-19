6c09114b342df186a831d4227c89b6aa582c18c3
manizio <92751858+manizio@users.noreply.github.com>
2021-10-19 17:56:57 -0300

    Adicionado main.c
    telaPrincipal + telaSobre

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void){
+    telaSobre();
+    telaPrincipal();     
+}
+
+
+void telaPrincipal(void){
+    printf("----------------------------------------------------\n");
+    printf("SIG-GAMES | Alugue o seu jogo!\n");
+    printf("Escolha uma opcao:\n1. Listar Jogos\n2. Adicionar Jogo\n3. Remover Jogo\n4. Alugar Jogo\n5. Consultar Jogos Alugados\n6. Sobre\n7. Equipe\n0. Sair");
+    printf("----------------------------------------------------\n");
+}
+
+
+/*Funcionalidades: 
+Listar jogos
+    Detalhar jogo
+Adicionar jogo
+Remover jogo
+Alugar jogo
+Consultar jogos alugados
+
+*/
+
+void telaSobre(void){
+    printf("         -------------------------------------------------  \n");
+    printf("         |  Universidade Federal do Rio Grande do Norte  |  \n");
+    printf("         |      Centro de Ensino Superior do Serido      |  \n");
+    printf("         |    Departamento de Computacao e Tecnologia    |  \n");
+    printf("         |       Disciplina DCT1106 -- Programacao       |  \n");
+    printf("         |      Projeto Locadora de Jogos SIG-GAMES      |  \n");
+    printf("         |    Desenvolvido por: @manizio - Out, 2021     |  \n");
+    printf("         |                      @israelsilva282          |  \n");
+    printf("         -------------------------------------------------  \n");
+    printf("\n");
+    printf("  ---------------------------------------------------------------------  \n");
+    printf("  |                Projeto Locadora de Jogos SIG-GAMES                |  \n");
+    printf("  |      Projeto utilizado na disciplina DCT1106 - Programacao.       |  \n");
+    printf("  |   programa contem as funcoes basicas para essa etapa do projeto:  |  \n");
+    printf("  |      uma tela Sobre, uma tela Principal e uma tela de equipe.     |  \n");
+    printf("  |       Todas as funcoes demonstradas na tela principal sao as      |  \n");
+    printf("  |       necessarias para o funcionamento da Locadora de Jogos.      |  \n");
+    printf("  ---------------------------------------------------------------------  \n");
+
+}
